26e59d9bb7f2f87cc0ec4d85ae0808baf4d8d5b5
nexustheru <sehindel_19@hotmail.com>
2018-08-13 03:11:35 +0200

    new

---
+++ tibiaExtractor/Form1.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 24)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(405, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 62)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(405, 20)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(423, 24)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Assetfolder"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(423, 59)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Extractfoder"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 235)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(484, 23)
+        Me.ProgressBar1.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 216)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Ready"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(423, 88)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Extract"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'BackgroundWorker1
+        '
+        Me.BackgroundWorker1.WorkerReportsProgress = True
+        Me.BackgroundWorker1.WorkerSupportsCancellation = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(508, 270)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+End Class
---
+++ tibiaExtractor/Form1.vb
@@ -0,0 +1,163 @@
+﻿Imports System
+Imports System.ComponentModel
+Imports System.Diagnostics
+Imports System.Drawing
+Imports System.Drawing.Imaging
+Imports System.IO
+Imports System.Threading
+Imports System.Windows.Forms
+Imports SevenZip.Compression.LZMA
+
+Public Class Form1
+    Public Shared _spriteBuffer As MemoryStream
+    Public Shared counter As Integer
+    Dim thread As New Thread(AddressOf Start)
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Public Shared Sub SaveStreamToFile(ByVal stream As Stream, ByVal filename As String)
+        Using destination As Stream = File.Create(filename)
+            Write(stream, destination)
+        End Using
+    End Sub
+
+    Public Shared Sub Write(ByVal from As Stream, ByVal [to] As Stream)
+        Dim a As Integer = from.ReadByte()
+
+        While a <> -1
+            [to].WriteByte(CByte(a))
+            a = from.ReadByte()
+        End While
+    End Sub
+
+    Public Sub TExtract(ByVal file As String)
+
+        Dim finfo As FileInfo = New FileInfo(file)
+        Dim spriteBuffer As MemoryStream = New MemoryStream(&H100000)
+        Dim decoder As Decoder = New Decoder()
+        Dim filestream As FileStream = New FileStream(file, FileMode.Open)
+        Using reader As BinaryReader = New BinaryReader(filestream)
+            Dim input As Stream = reader.BaseStream
+
+            While reader.ReadByte() = 0
+            End While
+
+            reader.ReadUInt32()
+
+            While (reader.ReadByte() And &H80) = &H80
+            End While
+
+            decoder.SetDecoderProperties(reader.ReadBytes(5))
+            reader.ReadUInt64()
+            spriteBuffer.Position = 0
+            decoder.Code(input, spriteBuffer, input.Length - input.Position, &H100000, Nothing)
+            spriteBuffer.Position = 0
+
+            Try
+                SaveStreamToFile(spriteBuffer, Me.TextBox2.Text + "\" + finfo.Name & ".bmp")
+            Catch ex As Exception
+                Me.Label1.Text = ex.Message
+            End Try
+        End Using
+    End Sub
+
+    Public Sub Start()
+
+    Dim fileArray As String() = Directory.GetFiles(Me.TextBox1.Text, "*.lzma")
+
+        For i As Integer = 0 To fileArray.Length - 1
+
+            If fileArray(i).Contains("sprites-") Then
+
+                TExtract(fileArray(i))
+            End If
+            counter += 1
+            Me.ProgressBar1.Value = counter * 100 / fileArray.Length
+            Me.ProgressBar1.Update()
+
+        Next
+        thread.Abort()
+    End Sub
+
+    Public Shared Sub Extract(ByVal file As String)
+
+        Dim finfo As FileInfo = New FileInfo(file)
+        Dim spriteBuffer As MemoryStream = New MemoryStream(&H100000)
+        Dim decoder As Decoder = New Decoder()
+        Dim filestream As FileStream = New FileStream(file, FileMode.Open)
+        Using reader As BinaryReader = New BinaryReader(filestream)
+            Dim input As Stream = reader.BaseStream
+
+            While reader.ReadByte() = 0
+            End While
+
+            reader.ReadUInt32()
+
+            While (reader.ReadByte() And &H80) = &H80
+            End While
+
+            decoder.SetDecoderProperties(reader.ReadBytes(5))
+            reader.ReadUInt64()
+            spriteBuffer.Position = 0
+            decoder.Code(input, spriteBuffer, input.Length - input.Position, &H100000, Nothing)
+            spriteBuffer.Position = 0
+
+            Try
+                SaveStreamToFile(spriteBuffer, Form1.TextBox2.Text + "\" + finfo.Name & ".bmp")
+            Catch ex As Exception
+                Form1.Label1.Text = ex.Message
+            End Try
+        End Using
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If FolderBrowserDialog1.ShowDialog = DialogResult.OK Then
+            TextBox2.Text = FolderBrowserDialog1.SelectedPath
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If FolderBrowserDialog1.ShowDialog = DialogResult.OK Then
+            TextBox1.Text = FolderBrowserDialog1.SelectedPath
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Label1.Text = "Extracting ...."
+        'Me.BackgroundWorker1.RunWorkerAsync()
+        thread.Start()
+    End Sub
+
+
+    Private Sub BackgroundWorker1_ProgressChanged(sender As Object, e As System.ComponentModel.ProgressChangedEventArgs) Handles BackgroundWorker1.ProgressChanged
+        '' This event is fired when you call the ReportProgress method from inside your DoWork.
+        '' Any visual indicators about the progress should go here.
+        ProgressBar1.Value = e.ProgressPercentage
+        Me.Label1.Text = e.ProgressPercentage.ToString & "% complete."
+    End Sub
+
+
+    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        Me.BackgroundWorker1.CancelAsync()
+    End Sub
+
+
+    Private Sub BackgroundWorker1_RunWorkerCompleted(sender As Object, e As RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+        Me.Label1.Text = "Finished!"
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        Dim fileArray As String() = Directory.GetFiles(Me.TextBox1.Text, "*.lzma")
+
+        For i As Integer = 0 To fileArray.Length - 1
+
+            If fileArray(i).Contains("sprites-") Then
+
+                Extract(fileArray(i))
+            End If
+            counter += 1
+            BackgroundWorker1.ReportProgress(counter * 100 / fileArray.Length)
+        Next
+    End Sub
+
+End Class
